5d85bf9cb6f6b5fc153a2561c14ebb0bd90f8348
hasanniloy122-rgb <hasanniloy122@gmail.com>
2026-04-25 17:37:40 +0600

    installed react router, tailwind css and daisy ui with the project

---
+++ src/main.jsx
@@ -0,0 +1,22 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: "Homepage",
+  },
+  {
+    path: "/timeline",
+    element: "Timeline",
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
+);
